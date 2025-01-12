e9e115dcc3e533d087b0750d2941c40b4ad7dc49
student <student@ST11-214>
2025-01-12 15:06:14 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp_12.01.2025/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_12._01._2025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
